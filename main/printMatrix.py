c3d786a10f9bc9c592d34259e10f9cca5baa801a
yangmx <1744053286@qq.com>
2019-08-25 09:57:58 +0800

    coding

--- main/printMatrix.py
+++ main/printMatrix.py
@@ -29,7 +29,8 @@ def printMatrix(matrix):
 
 
 if __name__ == "__main__":
-    matrix = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]
+    # matrix = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]
+    matrix = [[1],[2]]
     for line in matrix:
         print("{}".format(line))
     print(printMatrix(matrix))

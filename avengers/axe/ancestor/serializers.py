4d507856e6724bc0f51939db726156f190c14f03
yangmx <1744053286@qq.com>
2019-11-27 20:36:11 +0800

    use bs

--- avengers/axe/ancestor/serializers.py
+++ avengers/axe/ancestor/serializers.py
@@ -1,36 +1,25 @@
 # coding=utf-8
-import ssl
-import urllib.request
-import html2text
+
 
 from rest_framework import serializers, fields
 from ancestor.models import Resource
 
 
-class ResourceSerializer(serializers.ModelSerializer):
+class ResourceSerializer(serializers.Serializer):
+    # index = fields.URLField(required=True)
+    # content = fields.CharField(required=False)
+
+    def is_valid(self, raise_exception=False):
+        return super(ResourceSerializer, self).is_valid(raise_exception=raise_exception)
+
+    def create(self, validated_data):
+        return Resource.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.index = validated_data.get("index", "")
+        instance.content = validated_data.get("content", "")
+        return instance
+
     class Meta:
         model = Resource
         field = ["index", "type", "content"]
-
-
-class ResourceCreateSerializer(serializers.Serializer):
-    index = fields.URLField(required=True)
-    web_content = fields.SerializerMethodField()
-
-    def get_web_content(self, instance):
-        print(instance)
-        context = ssl._create_unverified_context()
-        with urllib.request.urlopen(instance.get("index", ""), context=context) as response:
-            html = response.read()
-            print(html)
-            text = html2text.html2text(html=html.decode("utf-8", "ignore"))
-        return text
-
-    def is_valid(self, raise_exception=False):
-        return super(ResourceCreateSerializer, self).is_valid(raise_exception=raise_exception)
-
-    def create(self, validated_data):
-        pass
-
-    def update(self, instance, validated_data):
-        pass
--- avengers/axe/ancestor/views.py
+++ avengers/axe/ancestor/views.py
@@ -1,12 +1,17 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+import ssl
+import urllib.request
+import html2text
 import os
 
-from django.template.loader import get_template
+from django.shortcuts import render
+from django.db import transaction
+
 from rest_framework.response import Response
 from rest_framework import viewsets
 
-from ancestor.serializers import ResourceCreateSerializer
+from bs4 import BeautifulSoup
+
+from ancestor.models import Resource
 
 
 def index(request):
@@ -14,18 +19,32 @@ def index(request):
 
 
 class AncestorViewSet(viewsets.ViewSet):
-    def __init__(self):
-        self.model = None
+    queryset = Resource.objects
 
     def retrieve(self, request):
         # print(request.GET)
         return Response({"hello": "test"})
 
     def more(self, request):
-        serializer = ResourceCreateSerializer(data=request.data)
-        if serializer.is_valid():
-            print("content {}".format(serializer.validated_data.get("web_content")))
-            print("content-data {}".format(serializer.data))
-            return Response({"success": True})
-        else:
-            return Response({"success": False})
+        index = request.data.get("index", "")
+        web_content = self._get_web_content(index)
+        with transaction.atomic():
+            resource = Resource.objects.create(index=index, type=0, content=web_content)
+            # serializer = ResourceSerializer(instance=resource, data={'index': index, 'content': web_content})
+            # if serializer.is_valid():
+            #     serializer.save()
+            #     print("content-data {}".format(serializer.data))
+            # resource.save()
+            return Response({"success": True, "index": index, "content": web_content})
+
+    def _get_web_content(self, index):
+        context = ssl._create_unverified_context()
+        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0'}
+        req = urllib.request.Request(index, headers=headers)
+        text = None
+        with urllib.request.urlopen(req, context=context) as response:
+            html = response.read()
+            text = html2text.html2text(html=html.decode("utf-8", "ignore"), baseurl=index)
+            bs_text = BeautifulSoup(html, features="lxml").get_text().replace('\n', '')
+            print("bs_text {}".format(bs_text))
+        return text

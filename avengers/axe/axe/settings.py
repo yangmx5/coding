7471959f98370595097285ef32fa375333d195f2
yangmx <1744053286@qq.com>
2020-01-31 21:38:10 +0800

    fix:use local db

--- avengers/axe/axe/settings.py
+++ avengers/axe/axe/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'axe.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'local.sqlite3'),
     }
 }
 
